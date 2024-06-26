023358a634974747e756ae8aa2b606b4e3697617
jpaulrajredhat <jpaulraj@redhat.com>
2024-06-26 12:52:03 +0530

    streaming added

---
+++ app/spark_kafka_streaming/trace_streaming.py
@@ -0,0 +1,42 @@
+from pyspark.sql import SparkSession
+import pyspark.sql.functions as F
+from pyspark.sql.types import StructType, StructField, StringType, LongType, TimestampType
+
+KAFKA_BOOTSTRAP_SERVERS = "kafka:9092"
+KAFKA_TOPIC = "trace"
+
+SCHEMA = StructType([
+    StructField("ID EQP", LongType()),
+    StructField("DATA HORA", TimestampType()),
+    StructField("MILESEGUNDO", LongType()),
+    StructField("CLASSIFICAÇÃO", StringType()),
+    StructField("FAIXA", LongType()),
+    StructField("ID DE ENDEREÇO", LongType()),
+    StructField("VELOCIDADE DA VIA", StringType()),
+    StructField("VELOCIDADE AFERIDA", StringType()),
+    StructField("TAMANHO", StringType()),
+    StructField("NUMERO DE SÉRIE", LongType()),
+    StructField("LATITUDE", StringType()),
+    StructField("LONGITUDE", StringType()),
+    StructField("ENDEREÇO", StringType()),
+    StructField("SENTIDO", StringType())
+])
+
+
+spark = SparkSession.builder.appName("read_trace_topic").getOrCreate()
+
+# Reduce logging verbosity
+spark.sparkContext.setLogLevel("WARN")
+
+df_trace_stream = spark\
+    .readStream.format("kafka")\
+    .option("kafka.bootstrap.servers", KAFKA_BOOTSTRAP_SERVERS)\
+    .option("subscribe", KAFKA_TOPIC)\
+    .option("startingOffsets", "earliest")\
+    .load()
+
+df_trace_stream.writeStream \
+  .format("console") \
+  .option("truncate", "false") \
+  .start() \
+  .awaitTermination()
